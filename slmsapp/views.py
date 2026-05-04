752781f79c252c9e6a3d7bee868c0f2d50ff1422
Jahnavi Chowdhary <jahnavijanu836@gmail.com>
2026-05-04 14:06:48 +0530

    Add FIRSTPAGE view to display staff and leave counts

--- slmsapp/views.py
+++ slmsapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from slmsapp.models import Staff, Staff_Leave
 
-# Create your views here.
+def FIRSTPAGE(request):
+    empcount = Staff.objects.count()
+    dptcount = 0   # you can update later if you have department model
+    leavtypcount = Staff_Leave.objects.count()
+
+    results = Staff_Leave.objects.all().order_by('-id')[:5]
+
+    return render(request, 'index.html', {
+        'empcount': empcount,
+        'dptcount': dptcount,
+        'leavtypcount': leavtypcount,
+        'results': results
+    })
