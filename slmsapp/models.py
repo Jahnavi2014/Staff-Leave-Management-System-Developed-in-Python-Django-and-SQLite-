0c036927d4f20cc59ece189f932bdbf47352f029
Jahnavi Chowdhary <jahnavijanu836@gmail.com>
2026-05-04 13:12:24 +0530

    Fix USER tuple syntax in CustomUser model

--- slmsapp/models.py
+++ slmsapp/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    USER ={
-        (1,'admin'),
-        (2,'staff')
-    }
+    USER = (
+    (1, 'admin'),
+    (2, 'staff'),
+)
+    # USER ={
+    #     (1,'admin'),
+    #     (2,'staff')
+    # }
     user_type = models.CharField(choices=USER,max_length=50,default=1)
 
     profile_pic = models.ImageField(upload_to='media/profile_pic')
